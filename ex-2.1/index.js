56b3b06e2f02962be1658ddd839bb5d0fea6fcda
Wilson <wilson.luiz583@gmail.com>
2025-05-31 12:40:25 -0300

    commit-2

---
+++ ex-2.1/index.js
@@ -0,0 +1,44 @@
+// Exemplo 1.5 - Cálculo em dobro de um número(ex1_5.hmtl)
+
+/*
+
+const num = prompt("Número: ") //lê um número
+const dobro = num*2 // calcula o dobro
+alert("Dobro é: "+ dobro)
+
+*/
+
+//Exemplo 1.6 - Soma de dois números(ex1_6.html)
+
+/*
+
+const num1 = Number(prompt("1° Número: ")) //lê os números
+const num2 = Number(prompt("2° Número: ")) 
+const soma = num1 + num2 // calcula a soma
+alert(`A Soma é : ${soma}`) // exibe o resultado
+
+*/
+
+// Exemplo 1.7 - Cálculo do valor do jantar (ex1_7.hmtl)
+
+/*
+
+const jantar = Number(prompt("Valor do jantar R$: ")) //lê o valor do jantar
+const garcom = jantar *0.10 // Calcula variáveis de saída
+const total = jantar + garcom
+alert(`Taxa Garçom R$: ${garcom.toFixed(2)}\nTotal R$: ${total.toFixed(2)}`)
+
+*/
+
+// Exemplo 1.8 - Cáculo da duração de horasa de uma viagem (ex1_8.html)
+
+/*
+
+const dias = Number(prompt("N° Dias: ")) //lê os dados de entrada
+const horas = Number(prompt("N° Horas: "))
+const total = (dias * 24) + horas /// calcula a duração
+alert(`Total de Horas: ${total}`) // Exibe o total
+
+*/
+
+
